69205392e098ac9ddd8efd9d19466555fbcf9bd5
Grover Gonzalez <grover.gonzalez@ucb.edu.bo>
2025-07-16 12:07:07 -0400

    primer commit, agregando todas las tablas correspondientes de nuestra DB

---
+++ database/migrations/2025_07_16_153940_create_inventory_outputs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_outputs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_codigo')->constrained('items', 'codigo');
+            $table->integer('cantidad');
+            $table->integer('user_ci');
+            $table->foreign('user_ci')->references('ci')->on('users');
+            $table->foreignId('departamento_id')->constrained();
+            $table->timestamp('fecha');
+            $table->text('observacion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_outputs');
+    }
+};
---
+++ database/migrations/2025_07_16_154400_create_seal_numbers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seal_numbers', function (Blueprint $table) {
+            $table->id();
+            $table->string('numero_sello')->unique();
+            $table->integer('user_ci');
+            $table->foreign('user_ci')->references('ci')->on('users');
+            $table->timestamp('fecha')->useCurrent();
+            $table->text('observacion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seal_numbers');
+    }
+};
