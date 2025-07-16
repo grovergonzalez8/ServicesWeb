229507dedec814fa15a6b2a18ef184724436293d
Grover Gonzalez <grover.gonzalez@ucb.edu.bo>
2025-07-16 12:35:31 -0400

    configuraicon models/user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,57 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $primaryKey = 'ci';
+    public $incrementing = false;
+    protected $ketType = 'integer';
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'ci',
+        'nombre',
+        'pasword',
+        'role_id'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function role()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->belongsTo(Role::class);
+    }
+
+    public function inventoryEntries()
+    {
+        return $this->hasMany(InventoryEntry::class, 'user_ci', 'ci');
+    }
+
+    public function inventoryOutputs()
+    {
+        return $this->hasMany(InventoryOutput::class, 'user_ci', 'ci');
+    }
+
+    public function sealNumbers()
+    {
+        return $this->hasMany(SealNumber::class, 'user_ci', 'ci');
+    }
+
+    public function letterNumbers()
+    {
+        return $this->hasMany(LetterNumber::class, 'user_ci', 'ci');
+    }
+
+    public function reportes()
+    {
+        return $this->hasMany(Reporte::class, 'user_ci', 'ci');
     }
 }
