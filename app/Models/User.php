bcc691c90cab510cc9b756c51348adfd781bc336
Grover Gonzalez <grover.gonzalez@ucb.edu.bo>
2025-07-17 10:52:25 -0400

    agregando sessions en configuracion models/user

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Reporte::class, 'user_ci', 'ci');
     }
+
+    public function sessions()
+    {
+        return $this->hasMany(Session::class, 'user_id', 'ci');
+    }
 }
