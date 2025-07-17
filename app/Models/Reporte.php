867fcc6fa786e4191accf022d349fbe16c565d7e
Grover Gonzalez <grover.gonzalez@ucb.edu.bo>
2025-07-17 10:46:29 -0400

    configuracion models/reporte

---
+++ app/Models/Reporte.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reporte extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'item_codigo',
+        'user_ci',
+        'departamento_id',
+        'titulo',
+        'contenido',
+        'tipo',
+        'observacion'
+    ];
+
+    protected $casts = [
+        'fecha' => 'datetime'
+    ];
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class, 'item_codigo', 'codigo');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_ci', 'ci');
+    }
+
+    public function departamento()
+    {
+        return $this->belongsTo(Departamento::class);
+    }
+}
