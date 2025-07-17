e06b2d8bf7feee2c1e810ca24e1b6f064d1fa947
Grover Gonzalez <grover.gonzalez@ucb.edu.bo>
2025-07-17 10:54:24 -0400

    configuracion database/factories/inventoryentryfactory

---
+++ database/factories/InventoryEntryFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class InventoryEntryFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'item_codigo' => \App\Models\Item::factory(),
+            'cantidad' => $this->faker->numberBetween(1, 100),
+            'user_ci' => \App\Models\User::factory(),
+            'fecha' => $this->faker->dateTimeThisYear(),
+            'observacion' => $this->faker->sentence(),
+        ];
+    }
+}
