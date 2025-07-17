ec1923a468011245d0c35e5ee023f1c3d4ceeecb
Grover Gonzalez <grover.gonzalez@ucb.edu.bo>
2025-07-17 10:13:58 -0400

    configuracion models/item

---
+++ app/Models/Item.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'codigo';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = ['codigo', 'nombre_item', 'stock', 'unidad'];
+
+    public function inventoryEntries()
+    {
+        return $this->hasMany(InventoryEntry::class, 'item_codigo', 'codigo');
+    }
+
+    public function inventoryOutputs()
+    {
+        return $this->hasMany(InventoryOutput::class, 'item_codigo', 'codigo');
+    }
+
+    public function reportes()
+    {
+        return $this->hasMany(Reporte::class, 'item_codigo', 'codigo');
+    }
+}
