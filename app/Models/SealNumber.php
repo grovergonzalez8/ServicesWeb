f2fae094b2947066ffd981f715a05273e68d7853
Grover Gonzalez <grover.gonzalez@ucb.edu.bo>
2025-07-17 10:41:37 -0400

    configuracion models/sealnumber

---
+++ app/Models/SealNumber.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SealNumber extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'numero_sello',
+        'user_ci',
+        'observacion'
+    ];
+
+    protected $casts = [
+        'fecha' => 'datetime'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_ci', 'ci');
+    }
+}
