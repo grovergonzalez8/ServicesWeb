125a37b2b1a64f47ea2c1bf18b303e17c2c0b341
Grover Gonzalez <grover.gonzalez@ucb.edu.bo>
2025-07-17 11:38:57 -0400

    configuracion database/factories/departamentofactory

---
+++ database/factories/DepartamentoFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DepartamentoFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'nombre' => $this->faker->unique()->department(),
+            'descripcion' => $this->faker->sentence(),
+        ];
+    }
+}
