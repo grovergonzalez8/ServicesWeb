9fec79b60e78177ba6b9fa5eff9ef59207d58772
Grover Gonzalez <grover.gonzalez@ucb.edu.bo>
2025-07-31 10:59:03 -0400

    corrigiendo index de reportes

--- resources/views/reportes/index.blade.php
+++ resources/views/reportes/index.blade.php
@@ -2,30 +2,36 @@
 
 @section('content')
 <div class="container">
-    <h1 class="mb-4">Reporte de Entradas y Salidas</h1>
+    <h1 class="mb-4">Reportes de Inventario (Entradas y Salidas)</h1>
 
     <table class="table table-bordered">
         <thead>
             <tr>
+                <th>Nro.</th>
                 <th>Tipo</th>
-                <th>Fecha</th>
-                <th>Item</th>
-                <th>Departamento</th>
-                <th>Usuario</th>
+                <th>Código</th>
+                <th>Producto</th>
                 <th>Cantidad</th>
+                <th>Usuario</th>
+                <th>Departamento</th>
+                <th>Fecha</th>
             </tr>
         </thead>
         <tbody>
-            @foreach($reportes as $reporte)
-            <tr>
-                <td>{{ $reporte->tipo }}</td>
-                <td>{{ $reporte->fecha->format('Y-m-d H:i') }}</td>
-                <td>{{ $reporte->item->nombre ?? '-' }}</td>
-                <td>{{ $reporte->departamento->nombre ?? '-' }}</td>
-                <td>{{ $reporte->user->nombre ?? '-' }}</td>
-                <td>{{ $reporte->cantidad }}</td>
-            </tr>
-            @endforeach
+            @forelse ($reportes as $reporte)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $reporte->tipo }}</td>
+                    <td>{{ $reporte->item->codigo }}</td>
+                    <td>{{ $reporte->item->nombre }}</td>
+                    <td>{{ $reporte->cantidad }}</td>
+                    <td>{{ $reporte->user?->nombre ?? '-' }}</td>
+                    <td>{{ $reporte->departamento?->nombre ?? '-' }}</td>
+                    <td>{{ $reporte->fecha->format('d/m/Y') }}</td>
+                </tr>
+            @empty
+                <tr><td colspan="8">No hay reportes registrados.</td></tr>
+            @endforelse
         </tbody>
     </table>
 </div>
